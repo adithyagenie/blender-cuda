d6f38efddaadc7f66d4d39194f49d3f3c8a096e5
adithyagenie <adithyagenie@gmail.com>
2025-11-26 12:19:45 +0530

    push to nixos 25.11
    Signed-off-by: adithyagenie <adithyagenie@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Blender with CUDA";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems = {
       url = "github:nix-systems/default";
